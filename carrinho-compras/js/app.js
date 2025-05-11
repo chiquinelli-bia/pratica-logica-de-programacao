629172c87be6e9d7ae6bbc20fc8620b904fa5955
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2025-05-11 17:56:30 -0300

    adicionar função de cálculo do total da compra com base na seleção de produtos e itens do carrinho

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -1,9 +1,18 @@
-let produtos = document.getElementById('produto');
-let selecionado = produtos.value;
+function adicionar () {
+    let produtos = document.getElementById('produto');
+    let selecionado = produtos.value;
 
-let quantidade = document.getElementById('quantidade').value;
-const nomeProduto = selecionado.split(' - R$')[0];
-const preco = selecionado.match(/R\$(\d+)/)[1];
+    let quantidade = document.getElementById('quantidade').value;
+    const nomeProduto = selecionado.split(' - R$')[0];
+    const preco = selecionado.match(/R\$(\d+)/)[1];
+    let conta = quantidade * preco;
 
-let listaProdutos = document.getElementById('lista-produtos');
 
+    let listaProdutos = document.getElementById('lista-produtos');
+
+    listaProdutos.innerHTML = listaProdutos.innerHTML =`<section class="carrinho__produtos__produto"><span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R${preco}</span></section>`;
+
+    totalGeral = totalGeral + conta;
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.textContent = `R$ ${totalGeral}`;
+}
