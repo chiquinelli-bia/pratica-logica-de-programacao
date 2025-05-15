c04d8d02655930ae33c2734d7e5001e234326597
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2025-05-15 18:09:55 -0300

    adicionar botão e função para calcular os ingresso

--- ingresso/js/app.js
+++ ingresso/js/app.js
@@ -1,19 +1,46 @@
-// quantidade disponivel - o tipo.
-// botao de Comprar. 
-// alerta se for mais do q tiver disponivel.
-
-    let tiposIngresso = document.getElementById('tipo-ingresso');
-    let ingresso = tiposIngresso.value;
-
-    let quantidade = document.getElementById('qtd').value;
-
-    let dspPista = document.getElementById('qtd-pista').textContent;
-    let dspSuperior = document.getElementById('qtd-superior').textContent;
-    let dspInferior = document.getElementById('qtd-inferior').textContent;
-
-
 function comprar () {
-    
-    console.log(quantidade);
+    let tiposIngresso = document.getElementById('tipo-ingresso');
+    let quantidade = parseInt(document.getElementById('qtd').value);
 
+
+    if (tiposIngresso.value == 'pista') {
+        comprarPista();
+    } else if (tiposIngresso.value == 'superior') {
+        comprarSuperior();
+    } else {
+        comprarInferior();
+    }
+
+    function comprarPista () {
+        let quantidadePista = parseInt(document.getElementById('qtd-pista').textContent);
+        if(quantidade > quantidadePista) {
+           alert('Quantidade indísponivel.');
+        } else {
+            quantidadePista = quantidadePista - quantidade;
+            document.getElementById('qtd-pista').textContent = quantidadePista;
+            alert('Compra efetuada com sucesso!');
+        }
+    }
+    function comprarSuperior () {
+        let quantidadeSuperior = document.getElementById('qtd-superior').textContent;
+        if(quantidade > quantidadeSuperior) {
+           alert('Quantidade indísponivel.');
+        } else {
+            quantidadeSuperior = quantidadeSuperior - quantidade;
+            document.getElementById('qtd-superior').textContent = quantidadeSuperior;
+            alert('Compra efetuada com sucesso!');
+        }
+    }
+    function comprarInferior () {
+        let quantidadeInferior = document.getElementById('qtd-inferior').textContent;
+        if(quantidade > quantidadeInferior) {
+           alert('Quantidade indísponivel.');
+        } else {
+            quantidadeInferior = quantidadeInferior - quantidade;
+            document.getElementById('qtd-inferior').textContent = quantidadeInferior;
+            alert('Compra efetuada com sucesso!');
+        }
+    }
 }
+        
+        
