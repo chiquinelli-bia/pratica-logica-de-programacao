a86362f7d1af79d5acc8feed8c8eb46037de8117
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2025-05-15 16:21:40 -0300

    selecionar elementos.

---
+++ ingresso/js/app.js
@@ -0,0 +1,19 @@
+// quantidade disponivel - o tipo.
+// botao de Comprar. 
+// alerta se for mais do q tiver disponivel.
+
+    let tiposIngresso = document.getElementById('tipo-ingresso');
+    let ingresso = tiposIngresso.value;
+
+    let quantidade = document.getElementById('qtd').value;
+
+    let dspPista = document.getElementById('qtd-pista').textContent;
+    let dspSuperior = document.getElementById('qtd-superior').textContent;
+    let dspInferior = document.getElementById('qtd-inferior').textContent;
+
+
+function comprar () {
+    
+    console.log(quantidade);
+
+}
