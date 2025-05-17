23893f125e26a5ce2ddd58d25460467f5c1734ee
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2025-05-17 17:26:26 -0300

    Adiciona funcionalidade de inserção e exibição de nomes na lista

--- amigo-secreto/js/app.js
+++ amigo-secreto/js/app.js
@@ -1,4 +1,18 @@
-//guardar o nome do amigo do input nome-amigo
+//guardar o nome do amigo no input nome-amigo
+let listaNomes = [];
 //por meio do botao adicionar() guardar o nome em <p id="lista-amigos"></p>
+function adicionar() {
+    let nomes = document.getElementById('nome-amigo').value;
+    let nomesIncluidos = document.getElementById('lista-amigos').innerText;
+
+    listaNomes.push(nomes);
+    console.log(listaNomes);
+
+    exibirText('lista-amigos', listaNomes);
+}
+function exibirText(id, texto) {
+    let campo = document.getElementById(id);
+    campo.innerHTML = texto;
+}
 //no reiniciar() resetar os campos
 //no botao sortear() fazer um sortear o outro e exibir em <p id="lista-sorteio"></p>
