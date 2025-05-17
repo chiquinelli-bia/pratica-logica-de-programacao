1c89ac7f6fe43f6452fcc7329b4635c77756ea41
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2025-05-17 16:56:00 -0300

    Definir objetivos

---
+++ amigo-secreto/js/app.js
@@ -0,0 +1,4 @@
+//guardar o nome do amigo do input nome-amigo
+//por meio do botao adicionar() guardar o nome em <p id="lista-amigos"></p>
+//no reiniciar() resetar os campos
+//no botao sortear() fazer um sortear o outro e exibir em <p id="lista-sorteio"></p>
